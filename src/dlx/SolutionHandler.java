0b33bdd46c982fd2096ac1e257e0749c7c3fdf02
Rafal Szymanski <me@rafal.io>
2014-08-05 18:36:41 +0200

    style changes, didnt know it used tabs

--- src/dlx/SolutionHandler.java
+++ src/dlx/SolutionHandler.java
@@ -4,68 +4,68 @@ import dlx.DancingLinks.*;
 import java.util.*;
 import dlx.SudokuDLX.*;
 
-public interface SolutionHandler {
-	void handleSolution(List<DancingNode> solution);
+public interface SolutionHandler{
+    void handleSolution(List<DancingNode> solution);
 }
 
-class SudokuHandler implements SolutionHandler {
-	int size = 9;
-	
-	public void handleSolution(List<DancingNode> answer){
-		int[][] result = parseBoard(answer);
-		printSolution(result);
-	}
-	
-	private int[][] parseBoard(List<DancingNode> answer){
-		int[][] result = new int[size][size];
-		for(DancingNode n : answer){
-			DancingNode rcNode = n;
-			int min = Integer.parseInt(rcNode.C.name);
-			for(DancingNode tmp = n.R; tmp != n; tmp = tmp.R){
-				int val = Integer.parseInt(tmp.C.name);
-				if(val < min){
-					min = val;
-					rcNode = tmp;
-				}
-			}
-			int ans1 = Integer.parseInt(rcNode.C.name);
-			int ans2 = Integer.parseInt(rcNode.R.C.name);
-			int r = ans1 / size;
-			int c = ans1 % size;
-			int num = (ans2 % size) + 1;
-			result[r][c] = num;
-		}
-		return result;
-	}
-	
-	private void printSolution(int[][] result){
-		for(int i = 0; i < size; i++){
-			String ret = "";
-			for(int j = 0; j < size; j++){
-				ret += result[i][j] + " ";
-			}
-			System.out.println(ret);
-		}
-		System.out.println();
-	}
-	
-	public SudokuHandler(int boardSize){
-		size = boardSize;
-	}
-	
+class SudokuHandler implements SolutionHandler{
+    int size = 9;
+
+    public void handleSolution(List<DancingNode> answer){
+        int[][] result = parseBoard(answer);
+        printSolution(result);
+    }
+
+    private int[][] parseBoard(List<DancingNode> answer){
+        int[][] result = new int[size][size];
+        for(DancingNode n : answer){
+            DancingNode rcNode = n;
+            int min = Integer.parseInt(rcNode.C.name);
+            for(DancingNode tmp = n.R; tmp != n; tmp = tmp.R){
+                int val = Integer.parseInt(tmp.C.name);
+                if (val < min){
+                    min = val;
+                    rcNode = tmp;
+                }
+            }
+            int ans1 = Integer.parseInt(rcNode.C.name);
+            int ans2 = Integer.parseInt(rcNode.R.C.name);
+            int r = ans1 / size;
+            int c = ans1 % size;
+            int num = (ans2 % size) + 1;
+            result[r][c] = num;
+        }
+        return result;
+    }
+
+    private void printSolution(int[][] result){
+        for(int i = 0; i < size; i++){
+            String ret = "";
+            for(int j = 0; j < size; j++){
+                ret += result[i][j] + " ";
+            }
+            System.out.println(ret);
+        }
+        System.out.println();
+    }
+
+    public SudokuHandler(int boardSize){
+        size = boardSize;
+    }
+
 }
 
 class DefaultHandler implements SolutionHandler{
-	public void handleSolution(List<DancingNode> answer){
-		for(DancingNode n : answer){
-			String ret = "";
-			ret += n.C.name + " ";
-			DancingNode tmp = n.R;
-			while(tmp != n){
-				ret += tmp.C.name + " ";
-				tmp = tmp.R;
-			}
-			System.out.println(ret);
-		}
-	}
+    public void handleSolution(List<DancingNode> answer){
+        for(DancingNode n : answer){
+            String ret = "";
+            ret += n.C.name + " ";
+            DancingNode tmp = n.R;
+            while (tmp != n){
+                ret += tmp.C.name + " ";
+                tmp = tmp.R;
+            }
+            System.out.println(ret);
+        }
+    }
 }
